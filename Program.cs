d4973431a45a70033c71c3b2933bc9097ddf3ad1
Ankur Seth <iankurseth@gmail.com>
2020-10-31 06:33:11 +0530

    Shubhaarambh

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sharp_md
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
